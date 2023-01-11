faecbb3d273d564dd70baf5973ee2bfde820177f
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2023-01-11 09:31:10 -0500

    -

--- src/index.ts
+++ src/index.ts
@@ -11,35 +11,40 @@ import promptSync from 'prompt-sync'
 const prompt = promptSync()
 console.log('Rock-Paper-Scissor')
 console.log('You VS PC')
-const number = prompt(
+const gameString = prompt(
   'Please pick rock(1), paper(2) or scissor(3): '
 )
 const rock = 1
 const paper = 2
 const scissor = 3
-const min = 1
-const max = 3
-const yPC = Math.floor(Math.random() * max) + min
 try {
-  const yNumber = parseFloat(number)
-  if (isNaN(yNumber)) {
-    console.log('Invalid input')
+  const game = parseFloat(gameString)
+  if (isNaN(game)) {
+    throw new Error('not a number')
   }
-  else if (yNumber < 1 && yNumber > 3) {
-    console.log('Invalid input')
+  if (game <= 0) {
+    throw new Error('negative number')
   }
-  else {
-    if (yNumber === yPC) {
-      console.log('Tie!')
-    } else if (
-      (yPC === paper && yNumber === rock) ||
-      (yPC === scissor && yNumber === paper) ||
-      (yPC === rock && yNumber === scissor)
-    ) {
-      console.log('PC wins!')
-    } else {
-      console.log('You win!')
-    }
+  if (game > 3) {
+    throw new Error('Number Higher than 3')
+  }
+  // set min & max
+  const min = 1
+  const max = 3
+  // use random npm install --save random
+  const randomNum = Math.floor(Math.random() * max) + min
+  const robot = randomNum
+
+  if (robot === game) {
+    console.log('Tie!')
+  } else if (
+    (robot === paper && game === rock) ||
+    (robot === scissor && game === paper) ||
+    (robot === rock && game === scissor)
+  ) {
+    console.log('PC wins!')
+  } else {
+    console.log('You win!')
   }
 } catch (e) {
   console.log('Invalid input')
